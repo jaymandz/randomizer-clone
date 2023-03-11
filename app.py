199c8112991fe913e69c541a4dc47c9105fe2c7f
Jay Mandane <jvergel.mandane@gmail.com>
2023-03-11 09:41:15 +0800

    Began work on the list component

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,6 +13,19 @@ application.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
 db = SQLAlchemy(application)
 migrate = Migrate(application, db)
 
+class List(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+
+class ListItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    list_id = db.Column(db.Integer, db.ForeignKey('list.id'))
+    body = db.Column(db.String())
+
 @application.get('/')
 def index():
     return render_template('index.html')
+
+@application.get('/_get_all_lists')
+def get_all_lists():
+    return jsonify(db.session.execute(db.select(List)).all())
